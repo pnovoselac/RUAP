ee62aff92b82f0cb21b186440783a4351cc0bd49
Nikola <mdamjanovic2010@gmail.com>
2023-10-31 09:19:42 +0100

    dodaj promjene

--- ruap_lv1/ruap_lv1/Program.cs
+++ ruap_lv1/ruap_lv1/Program.cs
@@ -7,6 +7,8 @@ namespace ruap_lv1
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdrav iz aplikacije od Pauline N!");
+            Console.WriteLine("Nemamo broj indexa");
+
         }
     }
 }
